bd5b9d1895db07ac9da751ae7befbc33f90fbd78
Alex.Shen <alex.shen@wesoft.com>
2020-01-07 09:40:36 +0800

    add project file

---
+++ Podfile
@@ -0,0 +1,22 @@
+platform :ios, '9.0'
+
+target 'RXDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  pod 'RxSwift', '~> 5'
+  pod 'RxCocoa', '~> 5'
+  pod 'XCoordinator'
+  pod 'XCoordinator/RxSwift'
+  pod 'XCoordinator/Combine'
+  
+  target 'RXDemoTests' do
+    pod 'RxBlocking', '~> 5'
+    pod 'RxTest', '~> 5'
+  end
+
+  target 'RXDemoUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ RXDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RXDemo
+//
+//  Created by 沈海超 on 2020/1/6.
+//  Copyright © 2020 沈海超. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
